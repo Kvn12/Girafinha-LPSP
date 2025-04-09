b60b5f1854c3028805f4e92c1c148005e8d52789
Kevin <kevincaio.ms@outlook.com>
2025-04-09 17:37:43 -0300

    generate new password and new position

--- gerador_senha.js
+++ gerador_senha.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const password = 'digite_a_sua_senha_desejada'; // Sua senha desejada
+const password = 'girafinha@lpsp'; // Sua senha desejada
 
 bcrypt.hash(password, saltRounds, function(err, hash) {
   if (err) throw err;
